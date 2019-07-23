52f39336c446aa34da2cf084530b580ec4424e09
Bart Saelen <bart.saelen@geosolutions.be>
2019-07-23 10:18:25 +0200

    add first custom fields

--- ckanext/benap/plugin.py
+++ ckanext/benap/plugin.py
@@ -2,8 +2,9 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class BenapPlugin(plugins.SingletonPlugin):
+class BenapPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IDatasetForm)
 
     # IConfigurer
 
@@ -11,3 +12,52 @@ class BenapPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'benap')
+
+    # IDatasetForm custom schema for BENAP
+
+    def _modify_package_schema(self, schema):
+        schema.update({
+            'custom_text': [
+                toolkit.get_validator('ignore_missing'),
+                toolkit.get_converter('convert_to_extras')
+            ],
+            'dataset_location': [
+                toolkit.get_validator('ignore_missing'),
+                toolkit.get_converter('convert_to_extras')
+            ]
+        })
+        return schema
+
+    def create_package_schema(self):
+        schema = super(BenapPlugin, self).create_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def update_package_schema(self):
+        schema = super(BenapPlugin, self).update_package_schema()
+        schema = self._modify_package_schema(schema)
+        return schema
+
+    def show_package_schema(self):
+        schema = super(BenapPlugin, self).show_package_schema()
+        schema.update({
+            'custom_text': [
+                toolkit.get_converter('convert_from_extras'),
+                toolkit.get_validator('ignore_missing')
+            ],
+            'dataset_location': [
+                toolkit.get_converter('convert_from_extras'),
+                toolkit.get_validator('ignore_missing')
+            ]
+        })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return True
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
