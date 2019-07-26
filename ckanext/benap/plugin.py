b2460f56b8a4feb86d28159f4ca5dfb0f4e70c42
Bart Saelen <bart.saelen@geosolutions.be>
2019-07-26 16:40:17 +0200

    add _ontology_helper

--- ckanext/benap/plugin.py
+++ ckanext/benap/plugin.py
@@ -15,6 +15,16 @@ class BenapPlugin(plugins.SingletonPlugin):
                                   ('municipality', 'municipality'),
                                   ('other', 'other')]
 
+    def _ontology_helper(self, context):
+        helper_out = map_for_form_select([
+            ('http://publications.europa.eu/resource/authority/data-theme/TRAN', {
+                "en": "Transport",
+                "fr": "Transports",
+                "nl": "Vervoer"
+            })
+        ])
+        return helper_out
+
     # IConfigurer
 
     def update_config(self, config_):
@@ -22,5 +32,10 @@ class BenapPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'benap')
 
+    # ITemplateHelpers
+
     def get_helpers(self):
-        return {'benap_geographic_granularity_helper': lambda x: map_for_form_select(self.geographic_granularity_map)}
+        return {
+            'benap_geographic_granularity_helper': lambda context: map_for_form_select(self.geographic_granularity_map),
+            'benap_ontology_helper': self._ontology_helper
+        }
