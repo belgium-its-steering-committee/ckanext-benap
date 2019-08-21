ec3194881061b6f509f691ff4db2f8658f4b15c2
Jonathan De Brandt <jonniedeb@gmail.com>
2019-08-21 13:07:14 +0200

    adding aarea covered by publication, based on NUTS

--- ckanext/benap/plugin.py
+++ ckanext/benap/plugin.py
@@ -194,7 +194,177 @@ class BenapPlugin(plugins.SingletonPlugin):
                     }),
             ])
 
-
+        if ontology == "NUTS3_BE":
+            return map_for_form_select([
+                ('http://data.europa.eu/nuts/code/BE100', {
+                    "en": u"Arr. de Bruxelles-Capitale / Arr. van Brussel-Hoofdstad",
+                    "fr": u"Arr. de Bruxelles-Capitale / Arr. van Brussel-Hoofdstad",
+                    "nl": u"Arr. de Bruxelles-Capitale / Arr. van Brussel-Hoofdstad",
+                    "de": u"Arr. de Bruxelles-Capitale / Arr. van Brussel-Hoofdstad"
+                }),
+                    ('http://data.europa.eu/nuts/code/BE211', {
+                        "en": u"Arr. Antwerpen",
+                        "fr": u"Arr. Antwerpen",
+                        "nl": u"Arr. Antwerpen",
+                        "de": u"Arr. Antwerpen"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE212', {
+                        "en": u"Arr. Mechelen",
+                        "fr": u"Arr. Mechelen",
+                        "nl": u"Arr. Mechelen",
+                        "de": u"Arr. Mechelen"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE213', {
+                        "en": u"Arr. Turnhout",
+                        "fr": u"Arr. Turnhout",
+                        "nl": u"Arr. Turnhout",
+                        "de": u"Arr. Turnhout"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE221', {
+                        "en": u"Arr. Hasselt",
+                        "fr": u"Arr. Hasselt",
+                        "nl": u"Arr. Hasselt",
+                        "de": u"Arr. Hasselt"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE222', {
+                        "en": u"Arr. Maaseik",
+                        "fr": u"Arr. Maaseik",
+                        "nl": u"Arr. Maaseik",
+                        "de": u"Arr. Maaseik"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE223', {
+                        "en": u"Arr. Tongeren",
+                        "fr": u"Arr. Tongeren",
+                        "nl": u"Arr. Tongeren",
+                        "de": u"Arr. Tongeren"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE231', {
+                        "en": u"Arr. Aalst"
+                        "fr": u"Arr. Aalst",
+                        "nl": u"Arr. Aalst",
+                        "de": u"Arr. Aalst"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE232', {
+                        "en": u"Arr. Dendermonde",
+                        "fr": u"Arr. Dendermonde",
+                        "nl": u"Arr. Dendermonde",
+                        "de": u"Arr. Dendermonde"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE233', {
+                        "en": u"Arr. Eeklo",
+                        "fr": u"Arr. Eeklo",
+                        "nl": u"Arr. Eeklo",
+                        "de": u"Arr. Eeklo"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE234', {
+                        "en": u"Arr. Gent",
+                        "fr": u"Arr. Gent",
+                        "nl": u"Arr. Gent",
+                        "de": u"Arr. Gent"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE235', {
+                        "en": u"Arr. Oudenaarde",
+                        "fr": u"Arr. Oudenaarde",
+                        "nl": u"Arr. Oudenaarde",
+                        "de": u"Arr. Oudenaarde"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE236', {
+                        "en": u"Arr. Sint-Niklaas",
+                        "fr": u"Arr. Sint-Niklaas",
+                        "nl": u"Arr. Sint-Niklaas",
+                        "de": u"Arr. Sint-Niklaas"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE241', {
+                        "en": u"Arr. Halle-Vilvoorde",
+                        "fr": u"Arr. Halle-Vilvoorde",
+                        "nl": u"Arr. Halle-Vilvoorde",
+                        "de": u"Arr. Halle-Vilvoorde"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE242', {
+                        "en": u"Arr. Leuven",
+                        "fr": u"Arr. Leuven",
+                        "nl": u"Arr. Leuven",
+                        "de": u"Arr. Leuven"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE251', {
+                        "en": u"Arr. Brugge",
+                        "fr": u"Arr. Brugge",
+                        "nl": u"Arr. Brugge",
+                        "de": u"Arr. Brugge"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE252', {
+                        "en": u"Arr. Diksmuide",
+                        "fr": u"Arr. Diksmuide",
+                        "nl": u"Arr. Diksmuide",
+                        "de": u"Arr. Diksmuide"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE253', {
+                        "en": u"Arr. Ieper",
+                        "fr": u"Arr. Ieper",
+                        "nl": u"Arr. Ieper",
+                        "de": u"Arr. Ieper"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE254', {
+                        "en": u"Arr. Kortrijk",
+                        "fr": u"Arr. Kortrijk",
+                        "nl": u"Arr. Kortrijk",
+                        "de": u"Arr. Kortrijk"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE255', {
+                        "en": u"Arr. Oostende",
+                        "fr": u"Arr. Oostende",
+                        "nl": u"Arr. Oostende",
+                        "de": u"Arr. Oostende"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE256', {
+                        "en": u"Arr. Roeselare",
+                        "fr": u"Arr. Roeselare",
+                        "nl": u"Arr. Roeselare",
+                        "de": u"Arr. Roeselare"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE257', {
+                        "en": u"Arr. Tielt",
+                        "fr": u"Arr. Tielt",
+                        "nl": u"Arr. Tielt",
+                        "de": u"Arr. Tielt"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE258', {
+                        "en": u"Arr. Veurne",
+                        "fr": u"Arr. Veurne",
+                        "nl": u"Arr. Veurne",
+                        "de": u"Arr. Veurne"
+                    }),
+                    ('http://data.europa.eu/nuts/code/BE310', {
+                        "en": u"Arr. Nivelles",
+                        "fr": u"Arr. Nivelles",
+                        "nl": u"Arr. Nivelles",
+                        "de": u"Arr. Nivelles"
+                    }),
+                    ('http://data.europa.eu/nuts/code/321', {
+                        "en": u"Arr. Ath",
+                        "fr": u"Arr. Ath",
+                        "nl": u"Arr. Ath",
+                        "de": u"Arr. Ath"
+                    }),
+                    ('http://data.europa.eu/nuts/code/322', {
+                        "en": u"Arr. Charleroi",
+                        "fr": u"Arr. Charleroi",
+                        "nl": u"Arr. Charleroi",
+                        "de": u"Arr. Charleroi"
+                    }),
+                    ('http://data.europa.eu/nuts/code/323', {
+                        "en": u"Arr. Mons",
+                        "fr": u"Arr. Mons",
+                        "nl": u"Arr. Mons",
+                        "de": u"Arr. Mons"
+                    }),
+                    ('http://data.europa.eu/nuts/code/', {
+                        "en": u"Arr. Mouscron",
+                        "fr": u"Arr. Mouscron",
+                        "nl": u"Arr. Mouscron",
+                        "de": u"Arr. Mouscron"
+                    }),
+            ])
 
         elif ontology == "data-theme":
             return map_for_form_select([
