44be89d3b36d56c00d8f21ac8991d4fd0d9d946c
Bart Saelen <bart.saelen@geosolutions.be>
2020-01-15 10:31:25 +0100

    1.1.4

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com//ckanext-benap',
+    url='https://github.com/belgium-its-steering-committee/ckanext-benap/',
 
     # Author details
     author='''Bart Saelen''',
