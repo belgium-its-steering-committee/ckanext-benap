ccde300c078bc0b12a588a92d4affd4f9d1d8108
michiel <michelangelo.bernaerts@gmail.com>
2021-08-20 16:11:42 +0200

    remove override check_access

--- ckanext/benap/logic/auth/get.py
+++ ckanext/benap/logic/auth/get.py
@@ -5,8 +5,3 @@ def user_list(context, data_dict=None):
     """Check whether access to the user list is authorised. Restricted to admins only."""
     return {'success': False}
 
-
-def organization_show(context, data_dict=None):
-    """Allow everyone to access organization data (for translated organization data helper)"""
-    return {'success': True}
-
