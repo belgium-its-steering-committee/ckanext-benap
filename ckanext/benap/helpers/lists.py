0049945e04c79aa282279a7afbf378e26157dac9
Bart Saelen <bart.saelen@geosolutions.be>
2022-03-09 13:30:28 +0100

    tags translations

---
+++ ckanext/benap/helpers/lists.py
@@ -0,0 +1,180 @@
+# coding=utf-8
+
+NUTS1_BE = [
+            ('http://data.europa.eu/nuts/code/BE1', {
+                "en": u"RÉGION DE BRUXELLES-CAPITALE/BRUSSELS HOOFDSTEDELIJK GEWEST",
+                "fr": u"RÉGION DE BRUXELLES-CAPITALE/BRUSSELS HOOFDSTEDELIJK GEWEST",
+                "nl": u"RÉGION DE BRUXELLES-CAPITALE/BRUSSELS HOOFDSTEDELIJK GEWEST",
+                "de": u"RÉGION DE BRUXELLES-CAPITALE/BRUSSELS HOOFDSTEDELIJK GEWEST"
+            }),
+            ('http://data.europa.eu/nuts/code/BE2', {
+                "en": u"VLAAMS GEWEST",
+                "fr": u"VLAAMS GEWEST",
+                "nl": u"VLAAMS GEWEST",
+                "de": u"VLAAMS GEWEST"
+            }),
+            ('http://data.europa.eu/nuts/code/BE3', {
+                "en": u"RÉGION WALLONNE",
+                "fr": u"RÉGION WALLONNE",
+                "nl": u"RÉGION WALLONNE",
+                "de": u"RÉGION WALLONNE"
+            }),
+        ]
+
+
+GEOREFERENCING_METHOD = [
+            ('ALERT-C (LCL)', {
+                "en": u"ALERT-C (LCL)",
+                "fr": u"ALERT-C (LCL)",
+                "nl": u"ALERT-C (LCL)",
+                "de": u"ALERT-C (LCL)"
+            }),
+            ('Geocoordinates WGS84', {
+                "en": u"Geocoordinates WGS84",
+                "fr": u"Geocoordinates WGS84",
+                "nl": u"Geocoordinates WGS84",
+                "de": u"Geocoordinates WGS84"
+            }),
+            ('Geocoordinates ETRS89', {
+                "en": u"Geocoordinates ETRS89",
+                "fr": u"Geocoordinates ETRS89",
+                "nl": u"Geocoordinates ETRS89",
+                "de": u"Geocoordinates ETRS89"
+            }),
+            ('ISO 19148', {
+                "en": u"ISO 19148",
+                "fr": u"ISO 19148",
+                "nl": u"ISO 19148",
+                "de": u"ISO 19148"
+            }),
+            ('OpenLR', {
+                "en": u"OpenLR",
+                "fr": u"OpenLR",
+                "nl": u"OpenLR",
+                "de": u"OpenLR"
+            }),
+            ('Other', {
+                "en": u"Other",
+                "fr": u"Autre",
+                "nl": u"Andere",
+                "de": u"Andere"
+            })
+        ]
+
+
+DATASET_TYPE = [
+            ('Static road network data', {
+                "en": u"Static road network data",
+                "fr": u"Données routières statiques",
+                "nl": u"Statische weggegevens",
+                "de": u"Statische Straβendaten"
+            }),
+            ('Static traffic signs and regulations', {
+                "en": u"Static traffic signs and regulations",
+                "fr": u"Panneaux routiers et réglementations statiques",
+                "nl": u"Statische verkeerssignalisatie en verkeersregels",
+                "de": u"Statische Verkehrszeichen und -regelungen"
+            }),
+            ('Toll information', {
+                "en": u"Toll information",
+                "fr": u"Informations sur les péages",
+                "nl": u"Tol informatie",
+                "de": u"Mautangaben"
+            }),
+            ('Parking and rest area information', {
+                "en": u"Parking and rest area information",
+                "fr": u"Informations sur les aires de stationnement et de repos",
+                "nl": u"Parking en stopplaatsen informatie",
+                "de": u"Informationen zu Park- und Rastplätzen"
+            }),
+            ('Filling and charging stations', {
+                "en": u"Filling and charging stations",
+                "fr": u"Stations-services et de recharge",
+                "nl": u"Tankstations en laadpunten",
+                "de": u"Tankstellen und Ladestationen"
+            }),
+            ('Freight and logistics', {
+                "en": u"Freight and logistics",
+                "fr": u"Marchandises et logistique",
+                "nl": u"Goederen en logistiek",
+                "de": u"Waren und Logistik"
+            }),
+            ('Dynamic traffic signs and regulations', {
+                "en": u"Dynamic traffic signs and regulations",
+                "fr": u"Panneaux routiers et réglementations dynamiques",
+                "nl": u"Dynamische verkeerssignalisatie en verkeersregels",
+                "de": u"Dynamische Verkehrszeichen und -regelungen"
+            }),
+            ('Road work information', {
+                "en": u"Road work information",
+                "fr": u"Informations sur les travaux de voirie",
+                "nl": u"Wegenwerken informatie",
+                "de": u"Informationen zu Straβenbauarbeiten"
+            }),
+            ('Unexpected road events and conditions', {
+                "en": u"Unexpected road events and conditions",
+                "fr": u"Evénements et conditions de route imprévus",
+                "nl": u"Wegenincidenten en condities",
+                "de": u"Zufällige Straβenereignisse und –zustände"
+            }),
+            ('Road weather conditions', {
+                "en": u"Road weather conditions",
+                "fr": u"Conditions météorologiques routières",
+                "nl": u"Weercondities wegen",
+                "de": u"Straβenwetterszustände"
+            }),
+            ('Real-time traffic data', {
+                "en": u"Real-time traffic data",
+                "fr": u"Données en temps réel sur la circulation",
+                "nl": u"Real-time verkeersinformatie",
+                "de": u"Echtzeit-Verkehrsinformation"
+            }),
+            ('General information for trip-planning', {
+                "en": u"General information for trip-planning",
+                "fr": u"Informations générales pour la planification des déplacements",
+                "nl": u"Algemene informatie voor routeplanning",
+                "de": u"Allgemeine Informationen zu Routenplanung"
+            }),
+            ('Public transport: location information', {
+                "en": u"Public transport: location information",
+                "fr": u"Transports publics : information de localisation",
+                "nl": u"Geregeld vervoer: locatie informatie",
+                "de": u"Öffentlicher Verkehr: Standortinformation"
+            }),
+            ('Public transport: operational information', {
+                "en": u"Public transport: operational information",
+                "fr": u"Transports publics : informations opérationnelles",
+                "nl": u"Geregeld vervoer: operationele informatie",
+                "de": u"Öffentlicher Verkehr: operative Informationen"
+            }),
+            ('Public transport: fare and purchase information', {
+                "en": u"Public transport: fare and purchase information",
+                "fr": u"Transports publics : informations tarifaires et commerciales",
+                "nl": u"Geregeld vervoer: tarieven en ticket informatie",
+                "de": u"Öffentlicher Verkehr: Preis- und Kaufinformationen"
+            }),
+            ('Cycle network data', {
+                "en": u"Cycle network data",
+                "fr": u"Données sur le réseau cyclable",
+                "nl": u"Fietsnetwerk informatie",
+                "de": u"Daten zum Fahrradnetzwerk"
+            }),
+            ('Pedestrian network data', {
+                "en": u"Pedestrian network data",
+                "fr": u"Données sur le réseau piétonnier",
+                "nl": u"Voetgangersnetwerk informatie",
+                "de": u"Fuβwegenetz"
+            }),
+            ('Demand-responsive modes', {
+                "en": u"Demand-responsive modes",
+                "fr": u"Modes de transport à la demande",
+                "nl": u"Vraagafhankelijk vervoer",
+                "de": u"Bedarfsgesteuerte Verkehrsträger"
+            }),
+            ('Other', {
+                "en": u"Other",
+                "fr": u"Autre",
+                "nl": u"Andere",
+                "de": u"Andere"
+            })
+        ]
