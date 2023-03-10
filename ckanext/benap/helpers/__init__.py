3592cb80bb2212b07c5c5c6459bc2b8f381e1a6f
geoFeltenVanballenberghe <felten.vanballenberghe@geosolutions.be>
2023-03-10 11:21:55 +0100

    dictionary bump

--- ckanext/benap/helpers/__init__.py
+++ ckanext/benap/helpers/__init__.py
@@ -1330,7 +1330,7 @@ def get_organization_by_id(id):
     
     logging.error('Organization_start')
     for k,v in organization.items():
-        logging.error(k, v)
+        logging.error(str(type(k)) + " ->", str(type(v)))
     logging.error('Organization_stop')
 
     field = 'display_title_' + user_language()
