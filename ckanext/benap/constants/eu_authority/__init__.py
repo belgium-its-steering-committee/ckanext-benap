1abb68837e4421f529a5f29b2d715c28818d20b9
Michaël Dierick <michael.dierick@redpencil.io>
2025-07-31 21:53:20 +0200

    refactor codelists to separate files

---
+++ ckanext/benap/constants/eu_authority/__init__.py
@@ -0,0 +1,1 @@
+from .eu_countries import EU_AUTH_EU_COUNTRIES as EU_COUNTRIES
---
+++ ckanext/benap/constants/eu_authority/eu_countries.py
@@ -0,0 +1,170 @@
+EU_AUTH_EU_COUNTRIES = [
+    ('http://publications.europa.eu/resource/authority/country/BEL', {
+        "en": "Belgium",
+        "fr": "Belgique",
+        "nl": "België",
+        "de": "Belgien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/NLD', {
+        "en": "Netherlands",
+        "fr": "Pays-Bas",
+        "nl": "Nederland",
+        "de": "Niederlande"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/FRA', {
+        "en": "France",
+        "fr": "France",
+        "nl": "Frankrijk",
+        "de": "Frankreich"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/DEU', {
+        "en": "Germany",
+        "fr": "Allemagne",
+        "nl": "Duitsland",
+        "de": "Deutschland"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/LUX', {
+        "en": "Luxembourg",
+        "fr": "Luxembourg",
+        "nl": "Luxemburg",
+        "de": "Luxemburg"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/GBR', {
+        "en": "United Kingdom",
+        "fr": "Royaume-Uni",
+        "nl": "Verenigd Koninkrijk",
+        "de": "Vereinigtes Königreich"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/BGR', {
+        "en": "Bulgaria",
+        "fr": "Bulgarie",
+        "nl": "Bulgarije",
+        "de": "Bulgarien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/CZE', {
+        "en": "Czechia",
+        "fr": "Tchéquie",
+        "nl": "Tsjechië",
+        "de": "Tschechien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/DNK', {
+        "en": "Denmark",
+        "fr": "Danemark",
+        "nl": "Denemarken",
+        "de": "Dänemark"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/EST', {
+        "en": "Estonia",
+        "fr": "Estonie",
+        "nl": "Estland",
+        "de": "Estland"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/IRL', {
+        "en": "Ireland",
+        "fr": "Irlande",
+        "nl": "Ierland",
+        "de": "Irland"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/GRC', {
+        "en": "Greece",
+        "fr": "Grèce",
+        "nl": "Griekenland",
+        "de": "Griechenland"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/ESP', {
+        "en": "Spain",
+        "fr": "Espagne",
+        "nl": "Spanje",
+        "de": "Spanien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/HRV', {
+        "en": "Croatia",
+        "fr": "Croatie",
+        "nl": "Kroatië",
+        "de": "Kroatien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/ITA', {
+        "en": "Italy",
+        "fr": "Italie",
+        "nl": "Italië",
+        "de": "Italien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/CYP', {
+        "en": "Cyprus",
+        "fr": "Chypre",
+        "nl": "Cyprus",
+        "de": "Zypern"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/LVA', {
+        "en": "Latvia",
+        "fr": "Lettonie",
+        "nl": "Letland",
+        "de": "Lettland"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/LTU', {
+        "en": "Lithuania",
+        "fr": "Lituanie",
+        "nl": "Litouwen",
+        "de": "Litauen"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/HUN', {
+        "en": "Hungary",
+        "fr": "Hongrie",
+        "nl": "Hongarije",
+        "de": "Ungarn"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/MLT', {
+        "en": "Malta",
+        "fr": "Malte",
+        "nl": "Malta",
+        "de": "Malta"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/AUT', {
+        "en": "Austria",
+        "fr": "Autriche",
+        "nl": "Oostenrijk",
+        "de": "Österreich"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/POL', {
+        "en": "Poland",
+        "fr": "Pologne",
+        "nl": "Polen",
+        "de": "Polen"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/PRT', {
+        "en": "Portugal",
+        "fr": "Portugal",
+        "nl": "Portugal",
+        "de": "Portugal"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/ROU', {
+        "en": "Romania",
+        "fr": "Roumanie",
+        "nl": "Roemenië",
+        "de": "Rumänien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/SVN', {
+        "en": "Slovenia",
+        "fr": "Slovénie",
+        "nl": "Slovenië",
+        "de": "Slowenien"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/SVK', {
+        "en": "Slovakia",
+        "fr": "Slovaquie",
+        "nl": "Slowakije",
+        "de": "Slowakei"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/FIN', {
+        "en": "Finland",
+        "fr": "Finlande",
+        "nl": "Finland",
+        "de": "Finnland"
+    }),
+    ('http://publications.europa.eu/resource/authority/country/SWE', {
+        "en": "Sweden",
+        "fr": "Suède",
+        "nl": "Zweden",
+        "de": "Schweden"
+    }),
+]
