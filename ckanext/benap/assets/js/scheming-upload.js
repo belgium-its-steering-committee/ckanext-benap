42a5e48b15ecfc59bce2cc83700d25fb3d53e631
Ruben <ruben.de.facq@redpencil.io>
2025-12-05 17:23:27 +0100

    logo upload: show * for required
    This is quite a hacky fix, because there is a lot of brittle code that made the <span> containing the label disappear.
    
    Comment out the initialization code that sets the `field-logo-upload`. This already contains the expected text anyway.
    
    Remove some custom logic that set the text. Unclear what the purpose was here.
    It says it adjusts the label, but label is already correct...

--- ckanext/benap/assets/js/scheming-upload.js
+++ ckanext/benap/assets/js/scheming-upload.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
         $( "div[data-module-field_upload|='rtti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='rtti_upload_doc']" ).attr("data-module-upload_label") );
         $( "div[data-module-field_upload|='srti_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='srti_upload_doc']" ).attr("data-module-upload_label") );
         $( "div[data-module-field_upload|='sstp_upload_doc']>div.form-group>label" ).text( $("div[data-module-field_upload|='sstp_upload_doc']" ).attr("data-module-upload_label") );
-        $( "div[data-module-field_upload|='image_upload']>div.form-group>label" ).text( $("div[data-module-field_upload|='image_upload']" ).attr("data-module-upload_label") );
+        // This used to adjust the text, breaking adding the * if the field is required.
+        // For now the .text() call is removed. Further checks have to be made what this even does and if this is still needed. 
+        $( "div[data-module-field_upload|='image_upload']>div.form-group>label" ).attr("data-module-upload_label");
         $('head').append("<style>.remove-after::after{ content:None }</style>");
     },1000);
   });
