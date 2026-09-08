f65e6c04bebf27b62e6fde1f52aff926c9699cbf
Leander Lismond <36822732+l5r@users.noreply.github.com>
2026-09-08 09:02:19 +0200

    Sort the search suggestions
    Try to suggest the most relevant options.

--- ckanext/benap/assets/js/benap-select-autocomplete.js
+++ ckanext/benap/assets/js/benap-select-autocomplete.js
@@ -1,8 +1,10 @@
-this.ckan.module('benap-select-autocomplete', function($) {
+this.ckan.module('benap-select-autocomplete', function($, _) {
   return {
     initialize() {
       const settings = {
         width: 'resolve',
+        placeholder: _('Start typing to search all options'),
+        allowClear: true,
         matcher(term, text, option) {
 
           if (!term || term.trim() === '') {
@@ -15,6 +17,33 @@ this.ckan.module('benap-select-autocomplete', function($) {
           }
 
           return null;
+        },
+        sorter(items, _el, { term, }) {
+          const termRegExp = RegExp.escape(term)
+          const re = new RegExp(termRegExp, 'i');
+          const startRe = new RegExp(`^${termRegExp}`, 'i');
+          const endRe = new RegExp(`${termRegExp}$`, 'i');
+
+          function score(text) {
+            if (text.match(startRe)) {
+              return 3;
+            } else if (text.match(endRe)) {
+              return 2;
+            } else if (text.match(re)) {
+              return 1;
+            } else {
+              return 0;
+            }
+          }
+
+          return items.sort(({ text: a }, { text: b }) => {
+            const aScore = score(a), bScore = score(b);
+            if (aScore === bScore) {
+              return a.localeCompare(b);
+            } else {
+              return bScore - aScore;
+            }
+          })
         }
       };
 
